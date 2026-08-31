145933c109ea63652666607f6880794daec3e3e7
Makhmudkhuja <3110MMM3110@email.com>
2026-09-01 04:46:13 +0900

    feat: CRUD operations are observed

--- app.js
+++ app.js
@@ -41,16 +41,35 @@ app.set("view engine", "ejs"); // view engine bu ejs ekanligi korsatilyapdi
 // });
 
 app.post(`/create-item`, (req, res) => {
+  console.log(`User entered /create-item`);
   console.log(req.body);
-  res.json({ test: "Success" });
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("Something went wrong");
+    } else {
+      res.end("Sucessfully  added");
+    }
+  });
 });
 
-app.get(`/author`, (req, res) => {
-  res.render(`author`, { user: user });
-});
+// app.get(`/author`, (req, res) => {
+//   res.render(`author`, { user: user });
+// });
 
 app.get(`/`, function (req, res) {
-  res.render(`reja`);
+  console.log(`User entered /`);
+  db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("something went wrong");
+      } else {
+        res.render(`reja`, { items: data });
+      }
+    });
 });
 
 module.exports = app;
