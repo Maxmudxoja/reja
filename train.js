cf46173bf1ab700213afdfa3eb0f44d8001817a2
Makhmudkhuja <3110MMM3110@email.com>
2026-09-10 14:41:38 +0900

    Feat:E task bajarildi

--- train.js
+++ train.js
@@ -1,47 +1,50 @@
 //MIT Task lar
 
-//D
+//E-Task
+function getReverse(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(getReverse("hello"));
 
 // D-TASK
 
-// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin. MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.cola = cola;
+//   }
 
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.non = non;
-    this.lagmon = lagmon;
-    this.cola = cola;
-  }
+//   vaqt() {
+//     let d = new Date();
+//     return d.getHours() + ":" + d.getMinutes();
+//   }
 
-  vaqt() {
-    let d = new Date();
-    return d.getHours() + ":" + d.getMinutes();
-  }
+//   qoldiq() {
+//     console.log(
+//       `Hozir ${this.vaqt()}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`,
+//     );
+//   }
 
-  qoldiq() {
-    console.log(
-      `Hozir ${this.vaqt()}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`,
-    );
-  }
+//   sotish(mahsulot, soni) {
+//     this[mahsulot] -= soni;
+//     console.log(`[${this.vaqt()}] Sotildi: ${soni}ta ${mahsulot}`);
+//   }
 
-  sotish(mahsulot, soni) {
-    this[mahsulot] -= soni;
-    console.log(`[${this.vaqt()}] Sotildi: ${soni}ta ${mahsulot}`);
-  }
+//   qabul(mahsulot, soni) {
+//     this[mahsulot] += soni;
+//     console.log(`[${this.vaqt()}] Qabul qilindi: ${soni}ta ${mahsulot}`);
+//   }
+// }
 
-  qabul(mahsulot, soni) {
-    this[mahsulot] += soni;
-    console.log(`[${this.vaqt()}] Qabul qilindi: ${soni}ta ${mahsulot}`);
-  }
-}
+// const shop = new Shop(4, 5, 2);
+// shop.qoldiq();
 
-const shop = new Shop(4, 5, 2);
-shop.qoldiq();
+// shop.sotish("non", 3);
+// shop.qabul("cola", 4);
 
-shop.sotish("non", 3);
-shop.qabul("cola", 4);
-
-shop.qoldiq();
+// shop.qoldiq();
 
 // //Task C
 
