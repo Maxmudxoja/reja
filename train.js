83f178e36ddede6bfa7ab4141a5ecba2d84bb9fc
Makhmudkhuja <3110MMM3110@email.com>
2026-09-15 16:36:49 +0900

    Feat: MIT F task bajarildi

--- train.js
+++ train.js
@@ -1,5 +1,21 @@
 //MIT Task lar
 
+// Task F
+
+function findDoublers(str) {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(findDoublers("hello"));
+console.log(findDoublers("world"));
+
 //Task -G
 
 // function getHighestIndex(arr) {
